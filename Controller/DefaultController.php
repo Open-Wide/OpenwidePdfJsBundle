9b700c709d3f196cd44093fc76cd832624389f4d
Laurent Sabatier <laurent.sabatier@openwide.fr>
2015-12-22 16:11:23 +0100

    Activate files

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,11 +9,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route(name="openwide_pdfjs_viewer", path="view/pdf", options={"expose": true})
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return $this->render('OpenwidePdfJsBundle::viewer.html.twig');
+    }
+
+    /**
+     * @Route(name="openwide_pdfjs_waiting", path="wait/pdf", options={"expose": true})
+     */
+    public function waitAction()
+    {
+        return $this->render('OpenwidePdfJsBundle::waiting.html.twig');
     }
 }
